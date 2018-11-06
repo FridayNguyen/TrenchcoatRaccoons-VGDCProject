2f962d1b07942f61c588f3707246ed94d06288cb
William Jeon <iamwilly99@gmail.com>
2018-11-05 23:01:50 -0800

    added pickup detection

---
+++ Assets/pickup_detector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup_detector : MonoBehaviour {
+
+	// Use this for initialization
+    private bool pickedup = false;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        // Sets pickedup to true and removes the pickup if a raccoon hits it.
+        if(coll.gameObject.CompareTag("pickup"))
+        {
+            pickedup = true;
+            print(pickedup);
+            Destroy(coll.gameObject);
+        }
+    }
+}
