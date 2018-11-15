856c871ab63a3c1dc04fbdf9d2312e07a4b8475b
Feasting <andrewtnguyen49@gmail.com>
2018-11-15 01:34:34 -0800

    Destruction and random Generation
    Wrote destruction script, random gap generator of just 1 block, and random 2x1 levels.

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -6,8 +6,8 @@ public class PlatformGenerator : MonoBehaviour
 {
 
     public GameObject thePlatform;
+    public GameObject theFloatingPlatform;
     public Transform generationPoint;
-    public float distanceBetween;
 
     private float platformWidth;
 
@@ -22,8 +22,19 @@ public class PlatformGenerator : MonoBehaviour
     {
         if (transform.position.x < generationPoint.position.x)
         {
+            float distanceBetween = Random.Range(0, 2);
+            
+            float floatingPlatform = Random.Range(0, 3);
+
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
             Instantiate(thePlatform, transform.position, transform.rotation);
+
+            if (floatingPlatform != 0)
+            {
+                int randomHeight = Random.Range(2, 8);
+                Vector3 tempPosition = new Vector3(transform.position.x + theFloatingPlatform.GetComponent<BoxCollider2D>().size.x, transform.position.y + randomHeight, transform.position.z);
+                Instantiate(theFloatingPlatform, tempPosition, transform.rotation);
+            }
         }
     }
 }
