d3f2c1077664e48062e2a28c554aeef4e7b45a9e
Feasting <andrewtnguyen49@gmail.com>
2018-11-16 10:21:38 -0800

    More Random
    Updated random platform generation to make it a bit more random.

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -21,18 +21,28 @@ public class PlatformGenerator : MonoBehaviour
     void Update()
     {
         if (transform.position.x < generationPoint.position.x)
-        {
-            float distanceBetween = Random.Range(0, 2);
-            
-            float floatingPlatform = Random.Range(0, 3);
+        {   
+            //Variables
+            float distanceBetweenChance = Random.Range(0, 4);
+            float floatingPlatform = Random.Range(0, 2);
+            int distanceBetween = 0;
 
+            //One out of four chance will there be a hole in the floor
+            if (distanceBetweenChance == 0)
+            {
+                distanceBetween = 1;
+            }
+
+            //Floor Platform, distancebetween will vary due to random distance chance
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
             Instantiate(thePlatform, transform.position, transform.rotation);
 
+            //Random floating platforms 2/3 chance
             if (floatingPlatform != 0)
             {
-                int randomHeight = Random.Range(2, 8);
-                Vector3 tempPosition = new Vector3(transform.position.x + theFloatingPlatform.GetComponent<BoxCollider2D>().size.x, transform.position.y + randomHeight, transform.position.z);
+                int randomHeight = Random.Range(3, 8);
+                int randomWidth = Random.Range(2,4);
+                Vector3 tempPosition = new Vector3(transform.position.x + theFloatingPlatform.GetComponent<BoxCollider2D>().size.x + randomWidth, transform.position.y + randomHeight, transform.position.z);
                 Instantiate(theFloatingPlatform, tempPosition, transform.rotation);
             }
         }
