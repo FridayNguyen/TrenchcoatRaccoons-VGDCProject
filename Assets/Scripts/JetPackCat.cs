4912c89601ef3a2083197e116edb09e51ad66a42
Feasting <andrewtnguyen49@gmail.com>
2018-11-20 10:39:27 -0800

    Some commenting of code

--- Assets/Scripts/JetPackCat.cs
+++ Assets/Scripts/JetPackCat.cs
@@ -9,20 +9,18 @@ public class JetPackCat : MonoBehaviour
 
     private Vector3 originalPosition;
 
+    //Variables regarding movement of cat
     public float chargeSpeed;
-
     public float hoverSpeed;
-
     public float hoverVerticalLength;
 
+    //Player Detection
     public float playerRange;
-
     public LayerMask playerLayer;
 
+    //Boolean Variables to check if conditions are met
     public bool playerInRange;
-
     public bool isCharging;
-
     private bool hoverUp;
 
     // Use this for initialization
@@ -48,10 +46,12 @@ public class JetPackCat : MonoBehaviour
         //Not in range, so random movements
         else
         {
+            //Hover upwards if less than a certain distance from original
             if ((transform.position.y < (originalPosition.y + hoverVerticalLength)) && hoverUp)
             {
                 transform.Translate(Vector2.up * Time.deltaTime);
             }
+            //Hover downwards if less than a certain distance from original
             else
             {
                 hoverUp = false;
