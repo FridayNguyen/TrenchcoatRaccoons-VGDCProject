5e8b853eaad959db968c2471bf83e1f0969c6fe0
Feasting <andrewtnguyen49@gmail.com>
2018-11-20 19:12:38 -0800

    Bird created
    wave like bird created

---
+++ Assets/Scripts/BirdEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdEnemy : MonoBehaviour {
+
+    [SerializeField]
+    public float moveSpeed;
+
+    [SerializeField]
+    public float frequency;
+
+    [SerializeField]
+    public float magnitude;
+
+    Vector3 pos, localScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveLeft();
+    }
+
+    void MoveLeft()
+    {
+        pos -= transform.right * Time.deltaTime * moveSpeed;
+        transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+}
