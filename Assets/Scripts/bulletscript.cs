908053a15526005498fc12a5e2934f7c1d467421
William Jeon <iamwilly99@gmail.com>
2018-11-24 01:39:48 -0800

    Added bullet detection

---
+++ Assets/Scripts/bulletscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletscript : MonoBehaviour
+{
+    public float speed = 3f;        //speed of bullet
+    public float timer = 4f;        //time for bullet to despawn
+
+    // Use this for initialization
+    private void Awake()
+    {
+        Destroy(gameObject, timer);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/enemyhitdetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyhitdetector : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))      //Checks if the trigger is a bullet
+        {
+            Destroy(collision.gameObject);
+            BulletDetected();
+        }
+    }
+
+    //This method runs when a bullet collides with this object.
+    void BulletDetected()
+    {
+        Destroy(this.gameObject);
+    }
+}
