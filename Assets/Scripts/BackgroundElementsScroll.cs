6783dda32d5c664a8aec1a88b60e8cd49b858421
William Jeon <iamwilly99@gmail.com>
2018-12-01 23:39:15 -0800

    Added background

---
+++ Assets/Scripts/BackgroundElementsScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundElementsScroll : MonoBehaviour
+{
+
+    public float scrollspeed = 2f;
+
+    void FixedUpdate()
+    {
+        // Moves right at a certain speed
+        transform.Translate(Vector3.right * Time.deltaTime * scrollspeed);
+    }
+
+    void OnBecameInvisible()
+    {
+        Instantiate(gameObject, transform.position + new Vector3(38.2f,0f,0f), transform.rotation);
+    }
+}
