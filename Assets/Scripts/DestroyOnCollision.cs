0e10c8b7be9ce9f40cfeffae758c971943ce3f26
Feasting <andrewtnguyen49@gmail.com>
2018-12-02 11:01:37 -0800

    Spike
    spike wont rotate

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour {
 
+    public GameObject col;
+
+    /*void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        OnCollisionEnter2D(Collision2D col);
+    }*/
+
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "player")
+        if (col.gameObject.tag == "Player")
         {
             Destroy(col.gameObject);
         }
