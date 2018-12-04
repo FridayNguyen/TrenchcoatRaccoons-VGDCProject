8da2f9478b19192b316c00248ec3964d982e6ca6
Sean Chang <seantc@uci.edu>
2018-12-03 23:29:53 -0800

    Adjusted raccoon spawning position

--- Assets/Scripts/raccoonAdd.cs
+++ Assets/Scripts/raccoonAdd.cs
@@ -7,16 +7,18 @@ public class raccoonAdd : MonoBehaviour {
     public GameObject raccoon;
     private GameObject allCoons;
 
-
     public void SpawnRaccoon()
     {
         PlayerController playerController = GameObject.Find("PlayerController").GetComponent<PlayerController>();
         List<GameObject> allRaccoons = playerController.aliveRaccoonGameObjects;
         GameObject topRaccoon = allRaccoons[allRaccoons.Count - 1];
+        GameObject mainCamera = GameObject.Find("Main Camera");
 
         Quaternion rotation = transform.rotation;
         Transform parent = GameObject.Find("AllCoons-DoNotRename").transform;
-        Vector3 spawnPoint = topRaccoon.transform.GetChild(0).transform.position;
+        float x = topRaccoon.transform.GetChild(0).transform.position.x;
+        float y = mainCamera.transform.position.y + mainCamera.transform.localScale.y;
+        Vector3 spawnPoint = new Vector3(x, y); 
 
         allRaccoons.Add(Instantiate(raccoon, spawnPoint, rotation, parent));
 
